ca55bd6cd0211fda744dda416b5ea44394f78d7b
alban <alban.flouvat@epfedu.fr>
2025-05-26 16:19:06 +0200

    Initial commit

---
+++ app/src/test/java/fr/epf/min1/projet_android_de_chassey_flouvat/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package fr.epf.min1.projet_android_de_chassey_flouvat
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
