f4f071ecfe021840de4aa656f617f224b44f53fe
alban <alban.flouvat@epfedu.fr>
2025-05-31 19:07:49 +0200

    feat: lien avec les autres activités

--- app/src/main/java/fr/epf/min1/projet_android_de_chassey_flouvat/MainActivity.kt
+++ app/src/main/java/fr/epf/min1/projet_android_de_chassey_flouvat/MainActivity.kt
@@ -25,11 +25,22 @@ import com.bumptech.glide.Glide
 import fr.epf.min1.projet_android_de_chassey_flouvat.data.Product
 import fr.epf.min1.projet_android_de_chassey_flouvat.repository.ProductRepository
 import android.content.Intent
+import android.view.inputmethod.InputMethodManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import fr.epf.min1.projet_android_de_chassey_flouvat.data.CartManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+
+        fab.setOnClickListener {
+            val intent = Intent(this, CartActivity::class.java)
+            startActivity(intent)
+        }
 
         val accountCard = findViewById<MaterialCardView>(R.id.main_Account_Card)
 
@@ -91,12 +102,17 @@ class MainActivity : AppCompatActivity() {
                 AfficherProduits(produits.take(10), container)
 
                 searchButton.setOnClickListener {
+                    searchEditText?.clearFocus()
+                    val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(searchEditText.windowToken, 0)
                     val query = searchEditText.text.toString().trim()
                     val resultats = allProduits.filter { it.title.contains(query, ignoreCase = true) }
                     AfficherProduits(resultats, container)
                 }
                 searchEditText.setOnFocusChangeListener { _, hasFocus ->
                     if (hasFocus) {
+                        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                        imm.showSoftInput(searchEditText, InputMethodManager.SHOW_IMPLICIT)
                         container.removeAllViews()
                     }
                 }
@@ -114,6 +130,39 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.Home -> {
+                    if (this !is MainActivity) startActivity(Intent(this, MainActivity::class.java))
+
+                    searchEditText?.clearFocus()
+
+                    // 2. Cacher le clavier s'il est ouvert
+                    val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(searchEditText.windowToken, 0)
+
+                    // 3. Réafficher les 10 premiers produits
+                    AfficherProduits(allProduits.take(10), findViewById(R.id.main_product_container))
+
+                    true
+                }
+                R.id.Search -> {
+                    searchEditText?.requestFocus()
+                    val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.showSoftInput(searchEditText, InputMethodManager.SHOW_IMPLICIT)
+                    true
+                }
+                R.id.Scan -> {
+                    true
+                }
+                R.id.Account -> {
+                    true
+                }
+                else -> false
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -125,6 +174,7 @@ class MainActivity : AppCompatActivity() {
         val imageView = view.findViewById<ImageView>(R.id.product_image)
         val prixText = view.findViewById<TextView>(R.id.product_price)
         val cardView = view.findViewById<CardView>(R.id.product_card)
+        val addPanierView = view.findViewById<CardView>(R.id.ajout_au_panier)
 
         prixText.text = "${produit.price} €"
 
@@ -134,9 +184,19 @@ class MainActivity : AppCompatActivity() {
 
         // Attache le produit à la vue (via tag)
         cardView.tag = produit
+
+        cardView.setOnClickListener{
+            val intent = Intent(view.context, ProductDetailActivity::class.java)
+            intent.putExtra("PRODUCT_ID", produit.id)
+            view.context.startActivity(intent)
+        }
+        addPanierView.setOnClickListener {
+            CartManager.addProduct(produit)
+            Toast.makeText(view.context, "${produit.title} ajouté au panier", Toast.LENGTH_SHORT).show()
+        }
     }
 
-    private fun AfficherProduits(produits : List<Product>, container : LinearLayout){
+    fun AfficherProduits(produits : List<Product>, container : LinearLayout){
         container.removeAllViews()
         for (i in 0 until produits.size step 2) {
             val rowView = LayoutInflater.from(this).inflate(R.layout.item_product_row, container, false) as LinearLayout
